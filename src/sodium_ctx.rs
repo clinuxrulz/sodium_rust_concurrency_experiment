e07bfcb31a334f1547791479fc64b53803d295be
Clinton Selke <clinuxrulz@gmail.com>
2019-10-27 10:14:20 +1100

    wip end of transaction

--- src/sodium_ctx.rs
+++ src/sodium_ctx.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use std::sync::Mutex;
 use std::mem;
 use crate::node::Node;
+use crate::node::NodeData;
 
 pub struct SodiumCtx {
     data: Arc<Mutex<SodiumCtxData>>
@@ -32,19 +33,21 @@ impl SodiumCtx {
             data.transaction_depth = data.transaction_depth + 1;
         });
         let result = k();
-        let post =
+        let post_op =
             self.with_data(|data: &mut SodiumCtxData| {
                 data.transaction_depth = data.transaction_depth - 1;
-                if (data.transaction_depth == 0) {
-                    data.end_of_transaction();
+                if data.transaction_depth == 0 {
                     let mut post: Vec<Box<dyn FnMut()>> = Vec::new();
                     mem::swap(&mut post, &mut data.post);
-                    return post;
+                    return Some(post);
                 }
-                return Vec::new();
+                return None;
             });
-        for mut k in post {
-            k();
+        if let Some(post) = post_op {
+            self.end_of_transaction();
+            for mut k in post {
+                k();
+            }
         }
         return result;
     }
@@ -54,10 +57,42 @@ impl SodiumCtx {
         let data: &mut SodiumCtxData = l.as_mut().unwrap();
         k(data)
     }
-}
 
-impl SodiumCtxData {
-    pub fn end_of_transaction(&mut self) {
+    pub fn end_of_transaction(&self) {
+        loop {
+            let mut changed_nodes: Vec<Node> =
+                self.with_data(|data: &mut SodiumCtxData| {
+                    let mut changed_nodes: Vec<Node> = Vec::new();
+                    mem::swap(&mut changed_nodes, &mut data.changed_nodes);
+                    return changed_nodes;
+                });
+            if changed_nodes.is_empty() {
+                break;
+            }
+            for node in changed_nodes {
+                SodiumCtx::update_node(&node);
+            }
+        }
+    }
 
+    pub fn update_node(node: &Node) {
+        let dependencies: Vec<Node> =
+            node.with_data(|data: &mut NodeData| data.dependencies.clone());
+        // visit dependencies
+        for dependency in &dependencies {
+            let visit_it = dependency.with_data(|data: &mut NodeData| !data.visited);
+            if visit_it {
+                SodiumCtx::update_node(dependency);
+            }
+        }
+        // any dependencies changed?
+        let any_changed =
+            dependencies
+                .iter()
+                .any(|node: &Node| { node.with_data(|data: &mut NodeData| data.changed) });
+        //
+        if any_changed {
+            
+        }
     }
 }

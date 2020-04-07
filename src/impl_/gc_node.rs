ccec52ed4a32a40c1652ccbec313bfa79a828dfe
Clinton Selke <clinuxrulz@gmail.com>
2020-04-07 23:26:09 +1000

    wip gc node

--- src/impl_/gc_node.rs
+++ src/impl_/gc_node.rs
@@ -5,6 +5,7 @@ pub type Tracer = dyn FnMut(&GcNode);
 
 pub type Trace = dyn Fn(&mut Tracer) + Send + Sync;
 
+#[derive(PartialEq,Eq)]
 enum Color {
     Black,
     Gray,
@@ -32,14 +33,16 @@ pub struct GcCtx {
 }
 
 struct GcCtxData {
-    roots: Vec<GcNode>
+    roots: Vec<GcNode>,
+    to_be_freed: Vec<GcNode>
 }
 
 impl GcCtx {
     pub fn new() -> GcCtx {
         GcCtx {
             data: Arc::new(Mutex::new(GcCtxData {
-                roots: Vec::new()
+                roots: Vec::new(),
+                to_be_freed: Vec::new()
             }))
         }
     }
@@ -52,6 +55,143 @@ impl GcCtx {
 
     pub fn possible_root(&self, node: GcNode) {
         self.with_data(|data: &mut GcCtxData| data.roots.push(node));
+    }
+
+    pub fn collect_cycles(&self) {
+        self.mark_roots();
+        self.scan_roots();
+        self.collect_roots();
+    }
+
+    fn mark_roots(&self) {
+        let mut old_roots: Vec<GcNode> = Vec::new();
+        self.with_data(
+            |data: &mut GcCtxData|
+                std::mem::swap(&mut old_roots, &mut data.roots)
+        );
+        let mut new_roots: Vec<GcNode> = Vec::new();
+        for root in old_roots {
+            let color = root.with_data(|data: &mut GcNodeData| data.color);
+            if color == Color::Purple {
+                self.mark_gray(&root);
+                new_roots.push(root);
+            } else {
+                let free_it =
+                    root.with_data(
+                        |data: &mut GcNodeData| {
+                            data.buffered = false;
+                            data.color == Color::Black && data.ref_count == 0
+                        }
+                    );
+                if free_it {
+                    self.with_data(
+                        |data: &mut GcCtxData|
+                            data.to_be_freed.push(root)
+                    );
+                }
+            }
+        }
+        self.with_data(
+            |data: &mut GcCtxData|
+                std::mem::swap(&mut new_roots, &mut data.roots)
+        );
+    }
+
+    fn mark_gray(&self, node: &GcNode) {
+        let bail = node.with_data(|data: &mut GcNodeData| {
+            if data.color == Color::Gray {
+                return true;
+            }
+            data.color = Color::Gray;
+            false
+        });
+        if bail {
+            return;
+        }
+
+        let this = self.clone();
+        node.trace(|t: &GcNode| {
+            t.with_data(|data: &mut GcNodeData| data.ref_count = data.ref_count - 1);
+            this.mark_gray(t);
+        });
+    }
+
+    fn scan_roots(&self) {
+        let mut roots = Vec::new();
+        self.with_data(
+            |data: &mut GcCtxData|
+                std::mem::swap(&mut roots, &mut data.roots)
+        );
+        for root in &roots {
+            self.scan(root);
+        }
+        self.with_data(
+            |data: &mut GcCtxData|
+                std::mem::swap(&mut roots, &mut data.roots)
+        );
+    }
+
+    fn scan(&self, s: &GcNode) {
+        let bail = s.with_data(|data: &mut GcNodeData| data.color != Color::Gray);
+        if bail {
+            return;
+        }
+
+        let ref_count = s.with_data(|data: &mut GcNodeData| data.ref_count);
+
+        if ref_count > 0 {
+            self.scan_black(s);
+        } else {
+            s.with_data(|data: &mut GcNodeData| data.color = Color::White);
+            let this = self.clone();
+            s.trace(|t| {
+                this.scan(t);
+            });
+        }
+    }
+
+    fn scan_black(&self, s: &GcNode) {
+        s.with_data(|data: &mut GcNodeData| data.color = Color::Black);
+        let this = self.clone();
+        s.trace(|t| {
+            let color =
+                t.with_data(|data: &mut GcNodeData| {
+                    data.ref_count = data.ref_count + 1;
+                    data.color
+                });
+            if color != Color::Black {
+                this.scan_black(t);
+            }
+        });
+    }
+
+    fn collect_roots(&self) {
+        let white = Vec::new();
+        let mut roots = Vec::new();
+        self.with_data(|data: &mut GcCtxData| roots.append(&mut data.roots));
+        for root in roots {
+            root.with_data(|data: &mut GcNodeData| data.buffered = false);
+            self.collect_white(&root, &mut white);
+        }
+        for i in white {
+            i.free();
+        }
+        let mut to_be_freed = Vec::new();
+        self.with_data(|data: &mut GcCtxData| to_be_freed.append(&mut data.to_be_freed));
+        for i in to_be_freed {
+            i.free();
+        }
+    }
+
+    fn collect_white(&self, s: &GcNode, white: &mut Vec<GcNode>) {
+        if s.with_data(|data: &mut GcNodeData| data.color == Color::White && !data.buffered) {
+            s.with_data(|data: &mut GcNodeData| data.color = Color::Black);
+            let this = self.clone();
+            s.trace(|t| {
+                this.collect_white(t, white);
+            });
+            white.push(s.clone());
+        }
     }
 }
 
@@ -112,9 +252,9 @@ impl GcNode {
         });
     }
 
-    pub fn trace(&self, tracer: &mut Tracer) {
+    pub fn trace<TRACER: FnMut(&GcNode) + 'static>(&self, mut tracer: TRACER) {
         self.with_data(|data: &mut GcNodeData| {
-            (data.trace)(tracer);
+            (data.trace)(&mut tracer);
         });
     }
 }
